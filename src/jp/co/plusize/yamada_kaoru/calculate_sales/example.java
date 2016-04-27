aec4bf5ce538f96fcc201c6419bcfd5526fa17e7
uchiumi <labit.0w0@gmail.com>
2016-04-27 17:47:00 +0900

    処理内容3-1,2

--- src/jp/co/plusize/yamada_kaoru/calculate_sales/example.java
+++ src/jp/co/plusize/yamada_kaoru/calculate_sales/example.java
@@ -5,6 +5,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 
@@ -32,13 +33,13 @@ public class example {
 		}
 		catch(FileNotFoundException e){
             System.err.println("支店定義ファイルが存在しません");
-            System.exit(1);//エラー処理
+            System.exit(1);
 		}
 		catch (IOException e) {
 			 System.err.println("支店定義ファイルのフォーマットが不正です");
 		}
 		finally{
-//			System.out.println(brunchmap.entrySet());//確認
+//			System.out.println(brunchmap.entrySet());
 		}
 
 		HashMap<String,String> commap = new HashMap<String,String>();//商品マップ作成
@@ -50,7 +51,7 @@ public class example {
            BufferedReader br = new BufferedReader(fr);
 
            while((com = br.readLine()) != null) {
-        	//System.out.println(com);//ファイルの読み込み確認
+        	//System.out.println(com);
         	String[] mom = com.split(",");//コード,商品名で分割
     		//System.out.println(mom[0]);
     		//System.out.println(mom[1]);
@@ -63,26 +64,74 @@ public class example {
 	}
 	catch(FileNotFoundException e){
         System.err.println("支店定義ファイルが存在しません");
-        System.exit(1);//エラー処理
+        System.exit(1);
 	}
 	catch (IOException e) {
 		 System.err.println("支店定義ファイルのフォーマットが不正です");
 	}
 	finally{
-//		System.out.println(commap.entrySet());//確認
-	 }
-	    File file = new File(args[0]);
-	    File files[] = file.listFiles();{
-
-	    for (int i=0; i<files.length; i++) {
+//		System.out.println(commap.entrySet());
+	 }  
+	    
+	    
+	    ArrayList salelist = new ArrayList();//.rcdリスト
+	    
+	File file = new File(args[0]);
+	File files[] = file.listFiles();{  
+		
+	for (int i=0; i<files.length; i++) {
 //	        System.out.println(files[i]);
-	    	if(files[i].toString().endsWith(".rcd")){
-	        	System.out.println(files[i]);//rcdで終わるものだけ出力
-	        }
-	}
+		if(files[i].toString().endsWith(".rcd")){
+	    if(files[i].getName().toString().length()==12){
+	    	salelist.add(files[i]);
 	    }
+	}
+   }
+  }
+	System.out.println(salelist.get(0).toString().substring(30,8));
+	}
+	
 }
-}
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+//	        	int j = Short.parseShort(files[i].getName().substring(0,8));
+//	        	salelist.add(j);//数字のみいれたリスト
+//	        	System.out.println(salelist.size());//
+//	        	System.out.println(salelist.get(0));//rcdかつ12桁のもののリスト
+
+
+//	    	if(salelist.get(0) + salelist.size()-1 != salelist.get(salelist.size()-1)){
+//	    		System.out.println("売上ファイル名が連番になっていません");
+
+
+	        	
+//				FileReader sr = new FileReader(files[i]);
+//	            BufferedReader ssr = new BufferedReader(sr);
+//	            String sales;//売上集計ファイルから読み込む内容
+//	            while((sales = ssr.readLine()) != null) {
+//	            	System.out.println(sales);
+	  
+
+	    
+	        	
+	        	
+//	        	System.out.println(files[i].getName().substring(0,8));//数字部分のみ
+//	        	int j = Short.parseShort(files[i].getName().substring(0,8));//文字列→
+                  
+	        	  
+
+
 
 
 
